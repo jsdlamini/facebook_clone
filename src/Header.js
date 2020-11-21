bc85a7d0b6523602208900a097dd009bf93f4085
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-21 12:42:05 +0200

    header components set up

---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleOutlinedIcon from "@material-ui/icons/SupervisedUserCircleOutlined";
+import AddIcon from "@material-ui/icons/Add";
+import { Avatar, IconButton } from "@material-ui/core";
+
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ForumIcon from "@material-ui/icons/Forum";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="https://www.brandeps.com/logo-download/F/Facebook-logo-vector-01.svg" />
+
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+
+      <div className="header__center">
+        <div className="header__option  header__option--active ">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option   ">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleOutlinedIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>johnsJDSD</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
