ac4e49367bfd568ee0e0ed657e30b68cc8ab36b2
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-21 18:30:39 +0200

    Sory reel added

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import './storyReel.css'
+import React from "react";
+import Story from "./Story";
+import "./storyReel.css";
 
 function StoryReel() {
-    return (
-        <div className='storyReel'>
-            
-        </div>
-    )
+  return (
+    <div className="storyReel">
+      <Story
+        image="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/95423330_2905288372918763_3510296454855917568_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=TcV4u35TPIAAX9GnO77&_nc_ht=scontent.fmts1-1.fna&oh=8b4b8f271eb336a85e8b49c2bdacb329&oe=5FDFAF84"
+        profileSrc="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/95423330_2905288372918763_3510296454855917568_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=TcV4u35TPIAAX9GnO77&_nc_ht=scontent.fmts1-1.fna&oh=8b4b8f271eb336a85e8b49c2bdacb329&oe=5FDFAF84"
+        title="Mzesh"
+      />
+      <Story
+        image="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/84008352_2710259969052762_3240683958773481472_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ktv7buoimo4AX94K4UW&_nc_ht=scontent.fmts1-1.fna&oh=b9935b7d6f45f5352de1289d5a9f11c8&oe=5FDFAC07"
+        profileSrc="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/49539514_2023686984376734_7397749303826972672_n.jpg?_nc_cat=110&ccb=2&_nc_sid=8bfeb9&_nc_ohc=-7zC0NOC34IAX80Z4QL&_nc_ht=scontent.fmts1-1.fna&oh=384a68c47d6bfe76d33f5edac594b489&oe=5FE0446E"
+        title="Mr T"
+      />
+      <Story
+        image="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/99073827_3472063162807101_3274145396141588480_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=AmIkQREM8NoAX8MQraz&_nc_ht=scontent.fmts1-1.fna&oh=4fda2d4f1a8e4819357f235c7dc503b4&oe=5FE03260"
+        profileSrc="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-0/c0.0.206.206a/p206x206/3087_1155117159744_7835719_n.jpg?_nc_cat=109&ccb=2&_nc_sid=da31f3&_nc_ohc=SQGqCck1aKMAX-uyN6L&_nc_ht=scontent.fmts1-1.fna&tp=27&oh=17b5a94d1b6ac296075399662e0d5e1d&oe=5FDCFCB7"
+        title="Mr T"
+      />
+
+      {/* Repeated for Quick dev */}
+      <Story
+        image="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/95423330_2905288372918763_3510296454855917568_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=TcV4u35TPIAAX9GnO77&_nc_ht=scontent.fmts1-1.fna&oh=8b4b8f271eb336a85e8b49c2bdacb329&oe=5FDFAF84"
+        profileSrc="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/95423330_2905288372918763_3510296454855917568_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=TcV4u35TPIAAX9GnO77&_nc_ht=scontent.fmts1-1.fna&oh=8b4b8f271eb336a85e8b49c2bdacb329&oe=5FDFAF84"
+        title="Mzesh"
+      />
+      <Story
+        image="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/84008352_2710259969052762_3240683958773481472_o.jpg?_nc_cat=106&ccb=2&_nc_sid=e3f864&_nc_ohc=ktv7buoimo4AX94K4UW&_nc_ht=scontent.fmts1-1.fna&oh=b9935b7d6f45f5352de1289d5a9f11c8&oe=5FDFAC07"
+        profileSrc="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/49539514_2023686984376734_7397749303826972672_n.jpg?_nc_cat=110&ccb=2&_nc_sid=8bfeb9&_nc_ohc=-7zC0NOC34IAX80Z4QL&_nc_ht=scontent.fmts1-1.fna&oh=384a68c47d6bfe76d33f5edac594b489&oe=5FE0446E"
+        title="Mr T"
+      />
+      <Story
+        image="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-9/99073827_3472063162807101_3274145396141588480_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=AmIkQREM8NoAX8MQraz&_nc_ht=scontent.fmts1-1.fna&oh=4fda2d4f1a8e4819357f235c7dc503b4&oe=5FE03260"
+        profileSrc="https://scontent.fmts1-1.fna.fbcdn.net/v/t1.0-0/c0.0.206.206a/p206x206/3087_1155117159744_7835719_n.jpg?_nc_cat=109&ccb=2&_nc_sid=da31f3&_nc_ohc=SQGqCck1aKMAX-uyN6L&_nc_ht=scontent.fmts1-1.fna&tp=27&oh=17b5a94d1b6ac296075399662e0d5e1d&oe=5FDCFCB7"
+        title="Mr T"
+      />
+    </div>
+  );
 }
 
-export default StoryReel
+export default StoryReel;
