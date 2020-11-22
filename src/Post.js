9b3e4cc200a5628412a6dba4bcd0bb77b2a7cc1b
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-22 18:36:23 +0200

    Feed framing

---
+++ src/Post.js
@@ -0,0 +1,50 @@
+import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
+import React from "react";
+import "./Post.js";
+import "./Post.css";
+function Post({ profilePic, image, username, timestamp, message }) {
+  return (
+    <div className="post">
+      <div className="post__top">
+        <Avatar src={profilePic} className="post__avatar" />
+        <div className="post__topInfo">
+          <h1>{username}</h1>
+          <p>Timestamp....</p>
+        </div>
+        </div>
+        <div className="post__bottom">
+          <p> {message}</p>
+        </div>
+        <div className="post__image">
+          <img src={image}   />
+        </div>
+
+        <div className="post__options">
+          <div className="post__option">
+            <ThumbUpIcon />
+            <p>Like</p>
+          </div>
+          <div className="post__option">
+            <ChatBubbleOutlineIcon />
+            <p> Comment</p>
+          </div>
+          <div className="post__option">
+            <NearMeIcon />
+            <p> Share</p>
+          </div>
+          <div className="post__option">
+            <AccountCircleIcon />
+            <ExpandMoreOutlinedIcon />
+          </div>
+        </div>
+       
+    </div>
+  );
+}
+
+export default Post;
