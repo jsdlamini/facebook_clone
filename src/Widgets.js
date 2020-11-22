95e41c88ee665848bd0fb49738e3fdb217d842fb
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-22 20:11:30 +0200

    facebook story widget added

---
+++ src/Widgets.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Widgets.css";
+
+function Widgets() {
+  return (
+    <div
+      class="fb-page"
+      data-href="https://www.facebook.com/SwazilandComputerSociety"
+      data-tabs="timeline"
+      data-width="500px"
+      data-height="500px"
+      data-small-header="true"
+      data-adapt-container-width="true"
+      data-hide-cover="true"
+      data-show-facepile="true"
+      width="500"
+      height="100%"
+      style={{ border: "none", overflow: "hidden" }}
+      scrolling="no"
+      frameborder="0"
+      allowTransparency="true"
+      allow="encrypted-media"
+    >
+      <blockquote
+        cite="https://www.facebook.com/SwazilandComputerSociety"
+        class="fb-xfbml-parse-ignore"
+      >
+        <a href="https://www.facebook.com/SwazilandComputerSociety">
+          Swaziland Computer Society
+        </a>
+      </blockquote>
+    </div>
+  );
+}
+
+export default Widgets;
