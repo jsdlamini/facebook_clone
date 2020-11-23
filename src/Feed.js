e0a10e9d983eb63fbeb17fe4718ca0e4bc55cf95
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-23 07:00:09 +0200

    Connection to firebase db and read writes

--- src/Feed.js
+++ src/Feed.js
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
 import StoryReel from "./StoryReel";
+import db from "./firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+    // console.log(posts);
+  }, []);
+
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      <Post
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          profilePic={post.data.profilePic}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
+          message={post.data.message}
+        />
+      ))}
+      {/* <Post
         profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjidvk3DPBtaN3SXhqTDVhzve_yJEhYIE9xQ&usqp=CAU"
         message="Now this works"
         timestamp=""
         username="Time stamp here"
         image="https://image.shutterstock.com/image-photo/happy-multiethnic-family-sitting-on-600w-717416929.jpg"
-      />
-
+      /> */}
+      {/* 
       <Post
         profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjidvk3DPBtaN3SXhqTDVhzve_yJEhYIE9xQ&usqp=CAU"
         message="Now this works"
         timestamp=""
         username="Time stamp here"
         image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvw65--Bz3j2IRCkPfEvNNmAL_ghdwcP4BEQ&usqp=CAU"
-      />
+      /> */}
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -5,7 +5,6 @@ import NearMeIcon from "@material-ui/icons/NearMe";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
 import React from "react";
-import "./Post.js";
 import "./Post.css";
 function Post({ profilePic, image, username, timestamp, message }) {
   return (
@@ -14,35 +13,35 @@ function Post({ profilePic, image, username, timestamp, message }) {
         <Avatar src={profilePic} className="post__avatar" />
         <div className="post__topInfo">
           <h1>{username}</h1>
-          <p>Timestamp....</p>
+          {/* <p> {timestamp}</p> */}
+          <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
         </div>
-        </div>
-        <div className="post__bottom">
-          <p> {message}</p>
-        </div>
-        <div className="post__image">
-          <img src={image}   />
-        </div>
+      </div>
+      <div className="post__bottom">
+        <p> {message}</p>
+      </div>
+      <div className="post__image">
+        <img src={image} alt="" />
+      </div>
 
-        <div className="post__options">
-          <div className="post__option">
-            <ThumbUpIcon />
-            <p>Like</p>
-          </div>
-          <div className="post__option">
-            <ChatBubbleOutlineIcon />
-            <p> Comment</p>
-          </div>
-          <div className="post__option">
-            <NearMeIcon />
-            <p> Share</p>
-          </div>
-          <div className="post__option">
-            <AccountCircleIcon />
-            <ExpandMoreOutlinedIcon />
-          </div>
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
         </div>
-       
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p> Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p> Share</p>
+        </div>
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreOutlinedIcon />
+        </div>
+      </div>
     </div>
   );
 }
